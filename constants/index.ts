4b9c53b524bd5f1b3802fc49ca7b936e2fa0f823
yupabhishek <abhirock1946246@gmail.com>
2025-03-27 01:15:30 +0530

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,342 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+   
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+   
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/yup.abhishek/",
+  },
+   
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://x.com/pitpiat",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Pituiya My discord bot protfolio",
+    description:
+      'Pituiya is a powerful Discord bot platform offering automation, moderation, and interactive features. Users can explore bot statistics, public bot listings, and invite Pituiya to their servers. Developed by PITPIAT#5499, it enhances community engagement and server management seamlessly.',
+    image: "/projects/project-6.png",
+    link: "https://pitpiat.netlify.app/pituiya",
+  },
+  {
+    title: "html/css/js Portfolio",
+    description:
+      'Pituiya is a powerful Discord bot platform offering automation, moderation, and interactive features. Users can explore bot statistics, public bot listings, and invite Pituiya to their servers. Developed by PITPIAT#5499, it enhances community engagement and server management seamlessly.',
+    image: "/projects/project-5.png",
+    link: "https://pitpiat.netlify.app/",
+  },
+  {
+    title: "Add my bot site",
+    description:
+      'The Pituiya bot collection website showcases multiple Discord bots designed by PITPIAT. It allows users to explore and invite bots for enhanced server functionality. The site includes bot features, contact information, and easy integration for improved community management and engagement.',
+    image: "/projects/project-3.png",
+    link: "https://pitpiat.netlify.app/bot",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://www.youtube.com/@pitpiat",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://www.youtube.com/@pitpiat",
+      },
+
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/yup.abhishek/",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://x.com/pitpiat",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/yupabhishek/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "By me a coffee",
+        icon: null,
+        link: "https://www.buymeacoffee.com/yup.abhishek",
+      },
+       
+      {
+        name: "Gmail",
+        icon: null,
+        link: "mailto:abhirock1946245@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/yupabhishek",
+};
